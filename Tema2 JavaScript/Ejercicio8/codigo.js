5d88ea60486f02f3c18c95b540399b69dd1ff649
Fran <Fran_Diz@outlook.com>
2023-09-29 10:53:38 +0200

    ok

--- Tema2 JavaScript/Ejercicio8/codigo.js
+++ Tema2 JavaScript/Ejercicio8/codigo.js
@@ -1,5 +1,13 @@
 var palabra = "yo hago yoga hoy"
-for (var i = palabra.length; i>0; i-1){
+
+var palindromo = ""
+for (var i = palabra.length; i>=0;){
     console.log(palabra[i]);
+    i-=1
+    palindromo+=palabra[i]
 }
-console.log("i")
+if (palindromo==palabra){
+    console.log("Es palindroma");
+}    else{ 
+    console.log("No es palindroma");
+}
