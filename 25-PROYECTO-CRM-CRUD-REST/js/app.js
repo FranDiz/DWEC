df359db9f6a5befa997601bb06e2ef2e91f7a0bb
FranDiz <Fran_Diz@outlook.com>
2023-11-02 10:51:41 +0100

    ok

---
+++ 25-PROYECTO-CRM-CRUD-REST/js/app.js
@@ -0,0 +1,4 @@
+const columnaNombre = document.getElementById("#nombre");
+const columnaTelefono = document.getElementById("#telefono");
+const columnaEmpresa = document.getElementById("#empresa");
+const columnaAcciones = document.getElementById("#acciones");
