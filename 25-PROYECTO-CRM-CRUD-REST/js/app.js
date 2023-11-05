9b2aa55d74561f58c533a5443cc2bdff87ac864c
FranDiz <107588190+FranDiz@users.noreply.github.com>
2023-11-05 21:13:51 +0100

    Delete 25-PROYECTO-CRM-CRUD-REST directory

--- 25-PROYECTO-CRM-CRUD-REST/js/app.js
+++
@@ -1,5 +0,0 @@
-const columnaNombre = document.getElementById("#nombre");
-const columnaTelefono = document.getElementById("#telefono");
-const columnaEmpresa = document.getElementById("#empresa");
-const columnaAcciones = document.getElementById("#acciones");
-
