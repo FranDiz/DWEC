df4b13646d916c51dd17ce50670396d6b6b17fa1
FranDiz <Fran_Diz@outlook.com>
2023-11-17 10:56:09 +0100

    ok

--- 30-CLIMA/js/app.js
+++ 30-CLIMA/js/app.js
@@ -2,3 +2,17 @@ const ciudad = document.querySelector("#ciudad")
 const pais = document.querySelector("#pais")
 const boton = document.querySelector("submit")
 const formulario = document.querySelector("#formulario")
+
+formulario.addEventListener("submit",enviarFormulario)
+
+function enviarFormulario(e){
+    e.preventDefault()
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${ciudad.value}&lon=${pais.value}&appid=e1e1afb71c313ba74717e47741f02998`
+    fetch(url)
+    .then(data => data.json())
+    .then(data => console.log(data))
+}
+
+function mostrarHTML(tempMax,tempMin,temp){
+    
+}
