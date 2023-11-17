8fa4c9417a2fab78ab932870e4e2735aa303e6bb
FranDiz <Fran_Diz@outlook.com>
2023-11-17 12:59:41 +0100

    ok

--- 30-CLIMA/js/app.js
+++ 30-CLIMA/js/app.js
@@ -4,15 +4,34 @@ const boton = document.querySelector("submit")
 const formulario = document.querySelector("#formulario")
 
 formulario.addEventListener("submit",enviarFormulario)
+ciudad.addEventListener("blur", crearError)
 
 function enviarFormulario(e){
     e.preventDefault()
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${ciudad.value}&lon=${pais.value}&appid=e1e1afb71c313ba74717e47741f02998`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad.value},${pais.value}&appid=e1e1afb71c313ba74717e47741f02998`
     fetch(url)
     .then(data => data.json())
     .then(data => console.log(data))
+    .then(data => mostrarHTML())
 }
 
-function mostrarHTML(tempMax,tempMin,temp){
-    
+function campoInvalido(e){
+    e.preventDefault();
+    const errorExistente = e.target.parentElement.querySelector('.error');
+    if (errorExistente){
+        errorExistente.remove()
+    }
+    if (e.target.value === ""){
+        let error = document.createElement("p")
+        error.textContent = "El campo ciudad esta vacío"
+        error.classList.add('error', 'bg-red-600', 'text-white', 'p-2', 'text-center');
+        e.target.insertAdjacentElement('afterend', error);
+    }
+}
+
+function mostrarHTML(temp, tempMax, tempMin){
+    const campo = document.createElement('p')
+    campo.appendChild(temp)
+    campo.appendChild(tempMax)
+    campo.appendChild(tempMin)
 }
