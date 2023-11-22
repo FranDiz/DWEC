106e7afa37426186ff756a02c7e74b30ebc0430e
FranDiz <Fran_Diz@outlook.com>
2023-11-22 09:53:31 +0100

    ok

--- 30-CLIMA/js/app.js
+++ 30-CLIMA/js/app.js
@@ -2,26 +2,29 @@ const ciudad = document.querySelector("#ciudad")
 const pais = document.querySelector("#pais")
 const boton = document.querySelector("submit")
 const formulario = document.querySelector("#formulario")
+const resultado = document.querySelector("#resultado");
 
-formulario.addEventListener("submit",enviarFormulario)
-ciudad.addEventListener("blur", crearError)
+formulario.addEventListener("submit", enviarFormulario)
+ciudad.addEventListener("blur", campoInvalido)
 
-function enviarFormulario(e){
+function enviarFormulario(e) {
     e.preventDefault()
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad.value},${pais.value}&appid=e1e1afb71c313ba74717e47741f02998`
     fetch(url)
-    .then(data => data.json())
-    .then(data => console.log(data))
-    .then(data => mostrarHTML())
+        .then(data => data.json())
+        .then(data => console.log(data))
+        .then(data => {
+            mostrarClima(data)
+        })
 }
 
-function campoInvalido(e){
+function campoInvalido(e) {
     e.preventDefault();
     const errorExistente = e.target.parentElement.querySelector('.error');
-    if (errorExistente){
+    if (errorExistente) {
         errorExistente.remove()
     }
-    if (e.target.value === ""){
+    if (e.target.value === '') {
         let error = document.createElement("p")
         error.textContent = "El campo ciudad esta vacío"
         error.classList.add('error', 'bg-red-600', 'text-white', 'p-2', 'text-center');
@@ -29,9 +32,38 @@ function campoInvalido(e){
     }
 }
 
-function mostrarHTML(temp, tempMax, tempMin){
-    const campo = document.createElement('p')
-    campo.appendChild(temp)
-    campo.appendChild(tempMax)
-    campo.appendChild(tempMin)
+function mostrarClima(clima) {
+    const {
+        ,
+        main: { temp, temp_max, temp_min },
+    } = clima;
+
+    const temperatura = kelvintoCentigrados(temp);
+    const max = kelvintoCentigrados(temp_max);
+    const min = kelvintoCentigrados(temp_min);
+
+    const nombreCiudad = document.createElement("p");
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add("font-bold", "text-2xl");
+
+    const actual = document.createElement("p");
+    actual.innerHTML = `${temperatura} &#8451;`;
+    actual.classList.add("font-bold", "text-6xl");
+
+    const tempMaxima = document.createElement("p");
+    tempMaxima.innerHTML = `Max: ${max} &#8451;`;
+    tempMaxima.classList.add("text-xl");
+
+    const tempMinima = document.createElement("p");
+    tempMinima.innerHTML = `Min: ${min} &#8451;`;
+    tempMinima.classList.add("text-xl");
+
+    const resultadoDiv = document.createElement("div");
+    resultadoDiv.classList.add("text-center", "text-white");
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+
+    resultado.appendChild(resultadoDiv);
 }
